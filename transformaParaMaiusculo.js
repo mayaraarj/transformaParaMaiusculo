516a878a6dfa9317544b58e2ea6f7b317f2f9199
mayara_Abreu <mayara.arj@gmail.com>
2022-04-18 15:35:57 -0300

    Criação do exercício

---
+++ transformaParaMaiusculo.js
@@ -0,0 +1,18 @@
+//Escreva uma função transformaParaMaiusculo(palavras). Essa função recebe como parâmetro um array de palavras. 
+//Ela deve retornar um array com a versão MAIÚSCULA de todas as palavras contidas no array passado como parâmetro.
+
+function transformaParaMaiusculo(palavras){
+
+    var array = [];
+ 
+    for(var i = 0; i < palavras.length;i++){
+ 
+        array.push(palavras[i].toUpperCase ());
+ 
+    }
+ 
+    return array;
+ 
+ }
+ 
+ console.log(transformaParaMaiusculo(["cachorro", "gato","periquito"]));
